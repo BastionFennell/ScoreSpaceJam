400bf3f7d15e2e89820611f0423625a53b965588
Bastion Fennell <bastion@brighthire.ai>
2022-12-17 01:15:18 -0600

    Make enemies spawn offscreen

--- scenes/EnemySpawner.gd
+++ scenes/EnemySpawner.gd
@@ -19,12 +19,39 @@ func _ready():
 
 	_spawn_enemy()
 
-func _spawn_enemy():
+func _is_coordinate_valid(coord):
+	var player = get_node("/root/World/Player")
+	var playerX = player.global_position.x - 1100
+	var playerY = player.global_position.y - 600
+	print (playerX)
+	print (playerY)
+
+	var inXRange = coord.x > playerX && coord.x < playerX + 2200
+	var inYRange = coord.x > playerY && coord.y < playerY + 1200
+
+	if (inXRange && inYRange):
+		return false
+
+	return true
+
+
+func _get_spawn_position():
 	var y = randf() * (height - 2 * margin) - margin
 	var x = randf() * (width - 2 * margin) - margin
 
+	while !_is_coordinate_valid(Vector2(x, y)):
+		y = randf() * (height - 2 * margin) - margin
+		x = randf() * (width - 2 * margin) - margin
+
+	return Vector2(x, y)
+
+
+
+func _spawn_enemy():
+	var coords = _get_spawn_position()
+
 	var enemy = Enemy.instance();
-	enemy.set_position(Vector2(x, y))
+	enemy.set_position(coords)
 	get_node("/root/World").call_deferred("add_child", enemy)
 
 	var timer = Timer.new()
