5f8906232169a979ad0f9f58e8b209e3e4bea4d0
Bastion Fennell <bastion@brighthire.ai>
2022-12-18 11:32:05 -0600

    Add globals and static inv GUI

---
+++ scenes/game/Globals.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+var inventory = {
+    "wood": 0,
+    "stone": 0
+}
+
+var days = 0
+var kills = 0
--- scenes/game/resources/tree/Tree.gd
+++ scenes/game/resources/tree/Tree.gd
@@ -3,3 +3,4 @@ extends "res://scenes/game/enemies/enemy.gd"
 func _ready():
 	speed = 0
 	drops = []
+	weights = []
