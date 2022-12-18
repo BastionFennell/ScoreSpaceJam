eb9276efbecb56ba1060bde0637a5930783862c3
Bastion Fennell <bastion@brighthire.ai>
2022-12-17 23:24:36 -0600

    Fix merge funkiness

--- scenes/game/weapons/shotgun.gd
+++ scenes/game/weapons/shotgun.gd
@@ -2,7 +2,8 @@ extends "res://scenes/game/weapons/Gun.gd"
 
 func _ready():
 	reload_time = 2
-	spread = 10
+	spread = 0.5
 	screen_shake = 0.3
+	count = 10
 
 	Bullet = preload("res://scenes/game/weapons/Shotgun Bullet.tscn")
