5d7af7d2b0d8888c1fb88d1658b56fa15ec251b6
Bastion Fennell <bastion@brighthire.ai>
2022-12-18 11:58:51 -0600

    Add town scenes and script

---
+++ town(second one).gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+var controller_mode = false
+var time = 0;
+var time_stopped = false;
+
+func _ready():
+	$AnimationPlayer.play("animations")
+
+func stop_time():
+	time_stopped = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("controller_mode"):
+		controller_mode = true
+	if Input.is_action_pressed("non_controller_mode"):
+		controller_mode = false
+
+	if !time_stopped:
+		time += delta
