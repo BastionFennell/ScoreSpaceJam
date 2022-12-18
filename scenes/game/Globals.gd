85e3afa2df68ea83c373179b7ee02d3539aa21e1
Bastion Fennell <bastion@brighthire.ai>
2022-12-18 11:53:41 -0600

    Add ability to pick up wood

--- scenes/game/Globals.gd
+++ scenes/game/Globals.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+signal inventory_updated
+
 var inventory = {
-    "wood": 0,
-    "stone": 0
+	"wood": 0,
+	"stone": 0
 }
 
 var days = 0
 var kills = 0
+
+func add_item(item, amount):
+	var new_amount = inventory[item] + amount if inventory[item] else amount
+
+	inventory[item] = new_amount
+	print(new_amount)
+	emit_signal("inventory_updated")
