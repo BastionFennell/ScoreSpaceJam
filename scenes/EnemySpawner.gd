aa3fc306e21c204e6cc96226bb231fa83fda10a9
Bastion Fennell <bastion@brighthire.ai>
2022-12-17 19:14:13 -0600

    Add infinite terrain generation

--- scenes/EnemySpawner.gd
+++ scenes/EnemySpawner.gd
@@ -6,45 +6,24 @@ export (int) var margin = 50
 export (float) var respawn_timer = 10.00
 export (float) var decay = 1.00
 
-var height = 0;
-var width = 0;
-var viewportWidth = 550
-var viewportHeight = 300
+var safe_radius = 150
 
 func _ready():
-	var bg = get_node("/root/World/Background")
-
-	height = bg.get_rect().size.x * bg.scale.x
-	width = bg.get_rect().size.y * bg.scale.y
-
 	_spawn_enemy()
 
-func _is_coordinate_valid(coord):
-	var player = get_node("/root/World/Player")
-	var playerX = player.global_position.x - viewportWidth / 2
-	var playerY = player.global_position.y - viewportHeight / 2
-
-	var inXRange = coord.x > playerX && coord.x < playerX + viewportWidth
-	var inYRange = coord.x > playerY && coord.y < playerY + viewportHeight
-
-	if (inXRange && inYRange):
-		return false
-
-	return true
-
-
 func _get_spawn_position():
-	var y = randf() * (height - 2 * margin) - margin
-	var x = randf() * (width - 2 * margin) - margin
+	var player = get_node("/root/World/Player")
 
-	while !_is_coordinate_valid(Vector2(x, y)):
-		y = randf() * (height - 2 * margin) - margin
-		x = randf() * (width - 2 * margin) - margin
+	var distance = randf() * 100 + safe_radius
+	var direction = randf() * 2 * PI
 
-	return Vector2(x, y)
+	var center = Vector2(player.global_position.x, player.global_position.y)
+	
+	return center + Vector2(distance, 0).rotated(direction)
 
 func _spawn_enemy():
 	var coords = _get_spawn_position()
+	print(coords)
 
 	var enemy = Enemy.instance();
 	enemy.set_position(coords)
@@ -61,6 +40,3 @@ func _spawn_enemy():
 		respawn_timer -= decay * randf()
 		if respawn_timer < 0.1:
 			respawn_timer = 0.1
-
-func _process(delta):
-	pass
---
+++ scenes/game/Terrain Generator.gd
@@ -0,0 +1,151 @@
+extends Node
+
+export (int) var radius = 200
+var tile_size = 10
+var rounded_radius = stepify(radius, tile_size)
+var steps = floor(rounded_radius / tile_size)
+var last_update_pos;
+
+var tilemap = [
+	{ "img": preload("res://assets/sprites/tiles/tile 1.png"), "chance": 0.6},
+	{ "img": preload("res://assets/sprites/tiles/tile 2.png"), "chance": 0.10},
+	{ "img": preload("res://assets/sprites/tiles/tile 3.png"), "chance": 0.15},
+	{ "img": preload("res://assets/sprites/tiles/tile 4.png"), "chance": 0.15},
+]
+
+var current_map = [[]]
+
+func _ready():
+	_initialize()
+
+func _get_tile():
+	var rand = randf();
+
+	for i in tilemap.size():
+		var tile = tilemap[i]
+		rand -= tile.chance
+		if rand <= 0:
+			return tile
+
+func _initialize():
+	var player = get_node("/root/World/Player")
+
+	# center
+	var c = Vector2(stepify(player.global_position.x, tile_size), stepify(player.global_position.y, tile_size))
+	last_update_pos = c
+
+	current_map.resize(steps * 2)
+	for i in current_map.size():
+		current_map[i] = []
+		current_map[i].resize(steps * 2)
+
+	for i in steps:
+		for j in steps:
+			var x = c.x - rounded_radius + i * tile_size
+			var y = c.y - rounded_radius + j * tile_size
+			current_map[i][j] = {"position": Vector2(x, y), "tile": _get_tile()}
+
+	for i in steps:
+		for j in steps:
+			var x = c.x - rounded_radius + i * tile_size
+			var y = c.y + j * tile_size
+			current_map[i][j + steps] = {"position": Vector2(x, y), "tile": _get_tile()}
+
+	for i in steps:
+		for j in steps:
+			var x = c.x + i * tile_size
+			var y = c.y - rounded_radius + j * tile_size
+			current_map[i + steps][j] = {"position": Vector2(x, y), "tile": _get_tile()}
+
+
+	for i in steps:
+		for j in steps:
+			var x = c.x + i * tile_size
+			var y = c.y + j * tile_size
+			current_map[i + steps][j + steps] = {"position": Vector2(x, y), "tile": _get_tile()}
+
+	_draw_tiles()
+
+func _draw_tile(i, j):
+	var world = get_node("/root/World")
+	var node = current_map[i][j]
+	var tile_sprite = Sprite.new()
+	tile_sprite.texture = node.tile.img
+	tile_sprite.position = node.position
+	tile_sprite.z_index = -1000
+
+	world.call_deferred("add_child", tile_sprite)
+	current_map[i][j].node = tile_sprite
+
+func _draw_tiles():
+	var world = get_node("/root/World")
+
+	for i in current_map.size():
+		for j in current_map[i].size():
+			_draw_tile(i, j)
+
+func _generate_y(c, row):
+	var y = c.y - rounded_radius + row * tile_size
+	for i in steps * 2:
+		var x = c.x + i * tile_size	- rounded_radius
+		current_map[i][row] = {"position": Vector2(x, y), "tile": _get_tile()}
+		_draw_tile(i, row);
+
+func _generate_x(c, column):
+	var x = c.x - rounded_radius + column * tile_size
+	current_map[column] = []
+	current_map[column].resize(steps * 2)
+	for j in steps * 2:
+		var y = c.y + j * tile_size	- rounded_radius
+		current_map[column][j] = {"position": Vector2(x, y), "tile": _get_tile()}
+		_draw_tile(column, j);
+
+func _update_tiles(c):
+	var size = current_map.size()
+	if c.x <= last_update_pos.x - tile_size:
+		for j in size:
+			current_map[size - 1][j].node.queue_free()
+
+		for i in size - 1:
+			current_map[size - (i + 1)] = current_map[size - (i + 2)]
+		
+		_generate_x(c, 0)
+
+	if c.x >= last_update_pos.x + tile_size:
+		for j in size:
+			current_map[0][j].node.queue_free()
+
+		for i in size - 1:
+			current_map[i] = current_map[i + 1]
+
+		_generate_x(c, size - 1)
+
+	if c.y <= last_update_pos.y - tile_size:
+		for i in size:
+			current_map[i][size - 1].node.queue_free()
+
+		for i in size:
+			for j in size - 1:
+				current_map[i][size - (j + 1)] = current_map[i][size - (j + 2)]
+
+		_generate_y(c, 0)
+
+	if c.y >= last_update_pos.y + tile_size:
+		for i in size:
+			current_map[i][0].node.queue_free()
+
+		for i in size:
+			for j in size - 1:
+				current_map[i][j] = current_map[i][j + 1]
+		
+		_generate_y(c, size - 1)
+
+
+func _process(delta):
+	var player = get_node("/root/World/Player")
+
+	# center
+	var c = Vector2(stepify(player.global_position.x, tile_size), stepify(player.global_position.y, tile_size))
+	if c.x <= last_update_pos.x - tile_size || c.x >= last_update_pos.x + tile_size || c.y <= last_update_pos.y - tile_size || c.y >= last_update_pos.y + tile_size:
+		_update_tiles(c)
+		last_update_pos = c
