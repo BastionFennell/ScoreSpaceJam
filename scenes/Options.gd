5065c5dbf6f085b78f931dcca42370ffb5cf5fe8
Bastion Fennell <bastion@brighthire.ai>
2022-12-19 12:48:42 -0600

    Fix hammergun, adjust sound

--- scenes/Options.gd
+++ scenes/Options.gd
@@ -1,6 +1,12 @@
 extends Button
 
+func _ready():
+	self.connect("focus_entered", self, "_on_focus_entered")
+
 func _pressed():
 	get_node("/root/Main/Options").visible = true
 
 	get_node("/root/Main/Options/Volume Sliders/Main Volume Section/Volume Slider").grab_focus()
+
+func _on_focus_entered():
+	get_node("/root/Main/Menu Sound").play()
--- scenes/Start Button.gd
+++ scenes/Start Button.gd
@@ -2,6 +2,10 @@ extends Button
 
 func _ready():
 	self.grab_focus()
+	self.connect("focus_entered", self, "_on_focus_entered")
 
 func _pressed():
 	get_tree().change_scene("res://Twilight.tscn")
+
+func _on_focus_entered():
+	get_node("/root/Main/Menu Sound").play()
