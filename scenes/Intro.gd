15a6dd08f6f7db78503637788a189d5e30bc07d2
Bastion Fennell <bastion@brighthire.ai>
2022-12-18 23:55:20 -0600

    Add bullet collision layer

--- scenes/Intro.gd
+++ scenes/Intro.gd
@@ -9,6 +9,5 @@ func _go_to_menu():
 	get_tree().change_scene("res://scenes/menu.tscn")
 
 func _process(_delta):
-	print("test?")
 	if Input.is_action_pressed("ui_cancel") || Input.is_action_pressed("ui_accept") || Input.is_action_pressed("shoot") || Input.is_action_pressed("shoot"):
 		_go_to_menu()
